365d9ed1eba7f4fc9100d2dcede2b686396e9dca
Adam Ochonicki <adam@fromonesrc.com>
2013-07-01 22:02:27 -0400

    Fix spec

--- spec/classes/libreoffice_spec.rb
+++ spec/classes/libreoffice_spec.rb
@@ -1,17 +1,11 @@
 require 'spec_helper'
 
 describe 'libreoffice' do
-  let(:facts) do
-    {
-      :boxen_home => '/opt/boxen'
-    }
-  end
-
   it do
     should contain_class('libreoffice')
 
     should contain_package('LibreOffice').with({
-      :source   => 'http://www.libreoffice.org/DownloadRedirect.php?target=http://download.documentfoundation.org/libreoffice/stable/4.0.4/mac/x86/LibreOffice_4.0.4_MacOS_x86.dmg'
+      :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.0.4/mac/x86/LibreOffice_4.0.4_MacOS_x86.dmg',
       :provider => 'appdmg'
     })
   end
