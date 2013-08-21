b3a32bf0fd0c346cd6184252f2a18a66a46e502f
Martin Gondermann <mgondermann@np4.net>
2013-08-21 14:16:54 +0200

    Fixed LibreOffice class Fixed LanguagePack class

--- manifests/languagepack.pp
+++ manifests/languagepack.pp
@@ -11,7 +11,7 @@
 #   }
 class libreoffice::languagepack ($locale = 'de') {
   package { 'LibreOffice LanguagePack':
-    provider => 'appdmg',
-    source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_${locale}.dmg'
+    provider => "appdmg",
+    source   => "http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_${locale}.dmg",
   }
 }
