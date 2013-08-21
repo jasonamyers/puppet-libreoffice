c89abbf4c2cd526568905c757a1cc29381e9f049
Martin Gondermann <mgondermann@np4.net>
2013-08-21 14:11:00 +0200

    Fixed Spec for languagepack

--- spec/classes/languagepack_spec.rb
+++ spec/classes/languagepack_spec.rb
@@ -3,23 +3,29 @@ require 'spec_helper'
 describe 'libreoffice::languagepack' do
   it do
     should contain_class('libreoffice::languagepack')
+  end
 
-    should contain_package('LibreOffice LanguagePack').with({
-      :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_de.dmg',
-      :provider => 'appdmg'
-    })
+  context "default language" do
+    it do
+      should contain_package('LibreOffice LanguagePack').with({
+       :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_de.dmg',
+       :provider => 'appdmg'
+      })
+    end
+  end
 
-    context "other language" do
-      let(:params) do
+  context "other language" do
+    let(:params) do
+      {
         :locale => 'en_GB'
-      end
+      }
+    end
 
-      it do
-        should contain_package('LibreOffice LanguagePack').with({
-          :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_en_GB.dmg',
-          :provider => 'appdmg'
-        })
-      end
+    it do
+      should contain_package('LibreOffice LanguagePack').with({
+        :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86_langpack_en_GB.dmg',
+        :provider => 'appdmg'
+      })
     end
   end
 end
