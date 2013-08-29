6bafe46ad8b180fbd00601f7bcbaa1fb69245c7a
Hugo Almeida <hugo@phybbit.com>
2013-08-29 21:42:44 +0900

    Fixed specs for 4.1.1

--- spec/classes/libreoffice_spec.rb
+++ spec/classes/libreoffice_spec.rb
@@ -5,7 +5,7 @@ describe 'libreoffice' do
     should contain_class('libreoffice')
 
     should contain_package('LibreOffice').with({
-      :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.0/mac/x86/LibreOffice_4.1.0_MacOS_x86.dmg',
+      :source   => 'http://download.documentfoundation.org/libreoffice/stable/4.1.1/mac/x86/LibreOffice_4.1.1_MacOS_x86.dmg',
       :provider => 'appdmg'
     })
   end
