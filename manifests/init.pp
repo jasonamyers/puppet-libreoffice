0f773791fb61a62dd8f9099a1cdf9bf6bd0dd493
dsisnero <dsisnero@users.noreply.github.com>
2014-12-12 09:49:12 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,9 @@
 #
 #   include libreoffice
 #   class { 'libreoffice':
-#     version => '4.2.5',
+#     version => '4.3.4',
 #   }
-class libreoffice($version='4.2.5') {
+class libreoffice($version='4.3.4') {
   package { "LibreOffice-${version}":
     provider => 'appdmg',
     source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86/LibreOffice_${version}_MacOS_x86.dmg",
